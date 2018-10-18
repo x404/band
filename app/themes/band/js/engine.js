abd0c8c720a35e29eeff51b1327263e5042a9d56
Aleksey <kronos2003@gmail.com>
2018-10-18 21:17:20 +0300

    Сделан вверх, добавлены векторные иконки

---
+++ app/themes/band/js/engine.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+
+	// карусель
+
+
+
+	// mobile-menu
+	$('#navbar').each(function(){
+		var $this = $(this),
+			$link = $('.navbar-toggle'),
+			$close = $('.close-menu'),
+
+			init = function(){
+				$link.on('click', openMenu);
+				$close.on('click', closeMenu);
+			},
+			openMenu = function(e){
+				e.preventDefault();
+				h = $(document).height();
+				$('body').addClass('o-menu');
+				$('#navbar').height(h);
+
+			},
+			closeMenu = function(e){
+				e.preventDefault();
+				$('body').removeClass('o-menu');
+				$('#navbar').height('auto');
+			};
+		init();
+	});	
+});
